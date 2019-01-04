759c95e35326b41bafa68a315517b3f76a1cbebc
Marios Igkiempor <mario@Marioss-MacBook-Pro.local>
2019-01-04 16:42:24 +0800

    testing /:name route

--- src/routes/CSVFiles.js
+++ src/routes/CSVFiles.js
@@ -1,15 +1,14 @@
-const express = require('express')
-const CSVFile = require('../models/CSV') // bring in the CSVFile model
-const router = express.Router()
+const express = require("express");
+const CSVFile = require("../models/CSV"); // bring in the CSVFile model
+const router = express.Router();
 
 // route to fetch all saved CSVFiles
-router.get('/', (req, res) => {
-  CSVFile.find()
-    .then(csvFiles => res.json(csvFiles))
-})
+router.get("/", (req, res) => {
+  CSVFile.find().then(csvFiles => res.json(csvFiles));
+});
 
 // route to add a CSVFiles
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   const newFile = new CSVFile({
     name: req.body.name,
     headings: req.body.headings,
@@ -29,27 +28,29 @@ router.post('/', (req, res) => {
     relations: req.body.relations,
     structure: req.body.structure,
     anomalies: req.body.anomalies
-  })
+  });
 
-  newFile.save()
-    .then(_ => res.json())
+  newFile.save().then(_ => res.json());
   // .then(json => console.log(json))
-})
+});
 
-router.get('/names/', (req, res) => {
+router.get("/names/", (req, res) => {
   CSVFile.find({}, (err, files) => {
-    let names = { list: [] }
-    files.forEach(file => names.list.push(file.name))
-    res.json(names)
-  })
-})
+    let names = { list: [] };
+    files.forEach(file => names.list.push(file.name));
+    res.json(names);
+  });
+});
 
-router.get('/:name', (req, res) => {
-  CSVFile.find({ "name": req.params.name }, (err, file) => {
-    if (err) { console.log(err); return; }
-    console.log("found")
-    res.json(file)
-  })
-})
+router.get("/:name", (req, res) => {
+  CSVFile.find({ name: req.params.name }, (err, file) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("found");
+    res.json(file[0]);
+  });
+});
 
-module.exports = router
+module.exports = router;
