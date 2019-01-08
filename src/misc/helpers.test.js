a52db8103ad88cfa00dd52ad31f0b95038bf264e
Marios Igkiempor <mario@Marioss-MacBook-Pro.local>
2019-01-08 21:57:34 +0800

    added flatten helper tests

--- src/misc/helpers.test.js
+++ src/misc/helpers.test.js
@@ -10,4 +10,62 @@ describe('isNull', () => {
     const answer = helpers.isNull(undefined)
     expect(answer).toBe(true)
   })
+
+  test('returns false when passed a positive number', () => {
+    const answer = helpers.isNull(123)
+    expect(answer).toBe(false)
+  })
+
+  test('returns false when passed a negative number', () => {
+    const answer = helpers.isNull(-123)
+    expect(answer).toBe(false)
+  })
+
+  test('returns false when passed a string', () => {
+    const answer = helpers.isNull('aaa')
+    expect(answer).toBe(false)
+  })
+
+  test('returns false when passed an array', () => {
+    const answer = helpers.isNull([1, 2, 3])
+    expect(answer).toBe(false)
+  })
+
+  test('returns false when passed an object', () => {
+    const answer = helpers.isNull({ a: 1, b: 'hello' })
+    expect(answer).toBe(false)
+  })
+
+  test('returns false when passed a boolean true', () => {
+    const answer = helpers.isNull(true)
+    expect(answer).toBe(false)
+  })
+
+  test('returns false when passed a boolean false', () => {
+    const answer = helpers.isNull(false)
+    expect(answer).toBe(false)
+  })
+})
+
+describe('flatten', () => {
+  test('empty array returns empty array', () => {
+    const answer = helpers.flatten([])
+    expect(answer).toEqual([])
+  })
+  test('1d singleton array returns the same array', () => {
+    const answer = helpers.flatten([1])
+    expect(answer).toEqual([1])
+  })
+  test('1d array returns the same array ', () => {
+    const answer = helpers.flatten([1, 2, 3])
+    expect(answer).toEqual([1, 2, 3])
+  })
+  test('2d singleton array returns the same array in 1d', () => {
+    const answer = helpers.flatten([[1], [2], [3]])
+    expect(answer).toEqual([1, 2, 3])
+  })
+  test('2d array returns the same array flattened in 1d', () => {
+    const answer = helpers.flatten([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    expect(answer).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9])
+  })
 })
