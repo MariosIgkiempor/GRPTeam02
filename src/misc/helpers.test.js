c77a9740f477af8dcb802c0ab67035ceffc82efe
Marios Igkiempor <mario@Marioss-MacBook-Pro.local>
2019-01-17 22:03:40 +0800

    removed tests for functions that no longer exist, added sort and power tests

--- src/misc/helpers.test.js
+++ src/misc/helpers.test.js
@@ -1,98 +1,78 @@
 const helpers = require('./helpers')
 
-describe('isNull', () => {
-  test('returns true when passed null', () => {
-    const answer = helpers.isNull(null)
-    expect(answer).toBe(true)
+describe('power', () => {
+  test('correctly exponentiates positive integers', () => {
+    expect(helpers.power(2)(3)).toBe(9)
+    expect(helpers.power(3)(9)).toBe(729)
   })
 
-  test('returns true when passed undefined', () => {
-    const answer = helpers.isNull(undefined)
-    expect(answer).toBe(true)
+  test('correctly exponentiates negative integers', () => {
+    expect(helpers.power(2)(-4)).toBe(16)
+    expect(helpers.power(3)(-5)).toBe(-125)
   })
 
-  test('returns false when passed a positive number', () => {
-    const answer = helpers.isNull(123)
-    expect(answer).toBe(false)
+  test('correctly deals with negative powers', () => {
+    expect(helpers.power(-2)(10)).toBe(0.01)
+    expect(helpers.power(-3)(50)).toBe(0.000008)
   })
 
-  test('returns false when passed a negative number', () => {
-    const answer = helpers.isNull(-123)
-    expect(answer).toBe(false)
+  test('correctly deals with floating point powers', () => {
+    expect(helpers.power(0.5)(64)).toBe(8)
+    expect(helpers.power(0.3)(10)).toBe(1.9952623149688795)
   })
 
-  test('returns false when passed a string', () => {
-    const answer = helpers.isNull('aaa')
-    expect(answer).toBe(false)
+  test('correctly exponentiates floating point numbers', () => {
+    expect(helpers.power(2)(1.5)).toBe(2.25)
+    expect(helpers.power(3)(10.5)).toBe(1157.625)
   })
 
-  test('returns false when passed an array', () => {
-    const answer = helpers.isNull([1, 2, 3])
-    expect(answer).toBe(false)
+  test('correctly deals with negative floating point powers', () => {
+    expect(helpers.power(-0.5)(64)).toBe(0.125)
+    expect(helpers.power(-0.5)(64)).toBe(0.125)
   })
 
-  test('returns false when passed an object', () => {
-    const answer = helpers.isNull({ a: 1, b: 'hello' })
-    expect(answer).toBe(false)
-  })
-
-  test('returns false when passed a boolean true', () => {
-    const answer = helpers.isNull(true)
-    expect(answer).toBe(false)
-  })
-
-  test('returns false when passed a boolean false', () => {
-    const answer = helpers.isNull(false)
-    expect(answer).toBe(false)
+  test('correctly exponentiates negative floating point numbers', () => {
+    expect(helpers.power(2)(-1.5)).toBe(2.25)
+    expect(helpers.power(3)(-10.5)).toBe(-1157.625)
   })
 })
 
-describe('flatten', () => {
+describe('iterative quicksort', () => {
   test('empty array returns empty array', () => {
-    const answer = helpers.flatten([])
-    expect(answer).toEqual([])
+    expect(helpers.qSort([])).toEqual([])
   })
-  test('1d singleton array returns the same array', () => {
-    const answer = helpers.flatten([1])
-    expect(answer).toEqual([1])
-  })
-  test('1d array returns the same array ', () => {
-    const answer = helpers.flatten([1, 2, 3])
-    expect(answer).toEqual([1, 2, 3])
-  })
-  test('2d singleton array returns the same array in 1d', () => {
-    const answer = helpers.flatten([[1], [2], [3]])
-    expect(answer).toEqual([1, 2, 3])
-  })
-  test('2d array returns the same array flattened in 1d', () => {
-    const answer = helpers.flatten([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    expect(answer).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9])
-  })
-})
 
-describe('isInteger', () => {
-  test('returns true when passed 0', () => {
-    const answer = helpers.isInteger(0)
-    expect(answer).toBe(true)
+  test('singleton arrays return the same singleton array', () => {
+    expect(helpers.qSort([1])).toEqual([1])
+    expect(helpers.qSort([Math.PI])).toEqual([Math.PI])
   })
-  test('returns true when passed a non-zero integer', () => {
-    const answer = helpers.isInteger(999)
-    expect(answer).toBe(true)
+
+  test('integer arrays sort correctly', () => {
+    expect(helpers.qSort([1, 5, 100, 47])).toEqual([1, 5, 47, 100])
+    expect(helpers.qSort([20, 4, 10, 9, 200, 20])).toEqual([
+      4,
+      9,
+      10,
+      20,
+      20,
+      200
+    ])
   })
-  test('returns true when passed a negative integer', () => {
-    const answer = helpers.isInteger(-5)
-    expect(answer).toBe(true)
-  })
-  test('returns false when passed a floating point number', () => {
-    const answer = helpers.isInteger(1.5)
-    expect(answer).toBe(false)
-  })
-  test('returns false when passed a negative floating point number', () => {
-    const answer = helpers.isInteger(-5.6)
-    expect(answer).toBe(false)
-  })
-  test('returns false when passed Math.PI', () => {
-    const answer = helpers.isInteger(Math.PI)
-    expect(answer).toBe(false)
+
+  test('float arrays sort correctly', () => {
+    expect(helpers.qSort([5.5, 5.5, 100.5, 47.5])).toEqual([
+      5.5,
+      5.5,
+      47.5,
+      100.5
+    ])
+    expect(helpers.qSort([20.2, 4.2, 10.2, 9.2, 200.2, 20.2])).toEqual([
+      4.2,
+      9.2,
+      10.2,
+      20.2,
+      20.2,
+      200.2
+    ])
   })
 })
