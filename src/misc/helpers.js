e5443ad1bcc0d041b62c04ed9ace283025d5fea3
Marios Igkiempor <mario@Marioss-MacBook-Pro.local>
2019-01-17 22:01:04 +0800

    refactored helpers file, fixed quicksort implementation

--- src/misc/helpers.js
+++ src/misc/helpers.js
@@ -1,66 +1,60 @@
-module.exports = {
-  isNull: x => x === null || x === undefined,
+const power = x => y => Math.pow(y, x)
 
-  flatten: a => [].concat(...a),
-
-  isInteger: x => typeof x === 'number' && isFinite(x) && Math.floor(x) === x,
-
-  isFloat: x => !!(x % 1),
-
-  power: x => y => Math.pow(y, x),
-
-  log: x => console.log(x),
-
-  sum: xs => xs.reduce((a, x) => (a = x ? a + x : a), 0),
-
-  mean: xs => this.sum(xs) / xs.length,
-
-  // quick sort implementation
-  qSort: function (xs) {
-    const partition = (array, low, high) => {
-      const pivot = array[high]
-      let i = low - 1
-      for (let j = low; j < high; j++) {
-        if (array[j] <= pivot) {
-          i++
-          const temp = array[i]
-          array[i] = array[j]
-          array[j] = temp
-        }
-      }
-
-      const temp = array[i + 1]
-      array[i] = array[high]
-      array[high] = temp
-
-      return i + 1
-    }
-
-    const qSortIterative = (array, low, high) => {
-      let stack = [] // auxiliary stack
-      let top = -1 // top of the stack index
-
-      stack[++top] = low
-      stack[++top] = high
-
-      while (top >= 0) {
-        high = stack[top--]
-        low = stack[top--]
-
-        let p = partition(array, low, high)
-
-        if (p - 1 > low) {
-          stack[++top] = low
-          stack[++top] = p - 1
-        }
-
-        if (p + 1 < high) {
-          stack[++top] = p + 1
-          stack[++top] = high
-        }
-      }
-    }
-
-    qSortIterative(xs, 0, xs.length - 1)
+const qSort = function (xs) {
+  const swap = (xs, i, j) => {
+    let temp = xs[i]
+    xs[i] = xs[j]
+    xs[j] = temp
   }
+  const partition = (array, low, high) => {
+    const pivot = array[high]
+    let pivotIndex = low
+    for (let j = low; j < high; j++) {
+      if (array[j] <= pivot) {
+        swap(array, j, pivotIndex)
+        pivotIndex++
+      }
+    }
+
+    swap(array, pivotIndex, high)
+    return pivotIndex
+  }
+
+  const qSortIterative = array => {
+    let stack = [] // auxiliary stack
+    let start = 0
+    let end = array.length - 1
+
+    stack.push({ start, end })
+
+    while (stack.length > 0) {
+      const top = stack.pop()
+      start = top.start
+      end = top.end
+
+      let pivot = partition(array, start, end)
+
+      if (pivot - 1 > start) {
+        stack.push({ start, end: pivot - 1 })
+      }
+
+      if (pivot + 1 < end) {
+        stack.push({ start: pivot + 1, end })
+      }
+    }
+  }
+
+  if (xs.length < 2) return xs
+  let output = [...xs]
+  qSortIterative(output, 0, xs.length - 1)
+  return output
+}
+
+module.exports = {
+  // Curried power function, for use with Rambda functional-style functions
+  power,
+
+  // iterative quick sort implementation,
+  // removes stack overflow errors possible with a recursive implementation
+  qSort
 }
