e5f232d8b11f079c833f31f2b7352bfa3953ab45
Xavier <xavier@XavierdeMacBook-Pro.local>
2019-03-08 16:50:44 +0800

    Start with js

---
+++ frontEnd/js/Dataset.js
@@ -0,0 +1,65 @@
+
+class HttpClient {
+
+  constructor() {
+
+    this.get = function(url, callback) {
+      const request = new XMLHttpRequest();
+      request.onreadystatechange = function() {
+        if (request.readyState === 4 && request.status === 200) {
+          callback(request.responseText);
+        }
+      };
+      request.open("GET", url, true);
+      request.setRequestHeader("Access-Control-Allow-Origin", "*");
+      request.send(null);
+    };
+  }
+}
+const client = new HttpClient();
+
+client.get(
+
+  "https://protected-tundra-24167.herokuapp.com/api/names",
+
+  makeListOfNames
+
+);
+
+function makeListOfNames(response) {
+  const list = document.createElement("ul");
+  list.style.listStyle = "none inside none";
+  list.style.padding = "10px"
+  list.style.fontSize = "30px"
+  list.style.lineHeight = "45px"
+
+  const names = JSON.parse(response).list;
+
+  console.log(names);
+
+  var i = 0;
+  for (const name of names) {
+    i = i%4;
+    const listItem = document.createElement("li");
+    listItem.innerHTML = name;
+    listItem.style.padding = "0px 10px"
+    switch (i) {
+      case 0:
+        listItem.style.backgroundColor = "#0094CB"
+        break;
+      case 1:
+        listItem.style.backgroundColor = "#FFFFFF"
+        break;
+      case 2:
+        listItem.style.backgroundColor = "#00AFD8"
+        break;
+      case 3:
+        listItem.style.backgroundColor = "#FFFFFF"
+        break;
+    }
+
+    list.appendChild(listItem);
+    i++;
+  }
+  document.querySelector("#output").appendChild(list);
+}
