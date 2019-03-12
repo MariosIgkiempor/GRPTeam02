247d073e3a79edb986349b8db9cf51a2456c107a
Marios Igkiempor <marioigkiempor@gmail.com>
2019-03-12 14:06:54 +0800

    Refactoring

--- frontEnd/js/Analysis.js
+++ frontEnd/js/Analysis.js
@@ -1,10 +1,5 @@
 const client = new HttpClient();
 
-client.get(
-  "https://protected-tundra-24167.herokuapp.com/api/names",
-  makeListOfNames
-);
-
 function getDatasetName() {
   var url = location.search;
   console.log(url);
@@ -21,43 +16,16 @@ client.get(
   makeDecision
 );
 
-function makeListOfNames(response) {
-  const list = document.createElement("ul");
-
-  // parse the response string into an array so we can loop over it
-
-  const names = JSON.parse(response).list;
-
-  console.log(names);
-
-  // loop over the list of names and add a li into the list for each name
-
-  for (const name of names) {
-    const listItem = document.createElement("li");
-
-    listItem.innerHTML = name;
-
-    list.appendChild(listItem);
-  }
-
-  // add the list to the output div
-
-  document.querySelector("#output").appendChild(list);
-}
+let dataset;
 
 // This function is for decision-making
-
 function makeDecision(response) {
-  const dataset = JSON.parse(response)[0]; // MAKE DATA OBJECT, DO NOT CHANGE!!!
+  dataset = JSON.parse(response)[0]; // MAKE DATA OBJECT, DO NOT CHANGE!!!
 
   console.log(dataset);
   const resultValue = document.createElement("body");
   const features = document.createElement("body");
-  var res =
-    "After analysis, the best Machine </br>learning algorithm of " +
-    getDatasetName() +
-    " is </br>";
-  var res2;
+  let methodType, bestMethod;
   //var features;
   //First of all we need to check: if(pattern != square(attributes), if 'if' return true, suggest user use Feature Selection Principal component Analysis):
   // console.log("suggest user use Feature Selection Principal component Analysis to preprocess dataset");
@@ -65,17 +33,17 @@ function makeDecision(response) {
   // Unsupervised Learning
   if (dataset.labelsRatio == 0) {
     console.log("Unsupervised Learning");
-    res2 = "Unsupervised Learning";
-    if (dataset.number - of - features < 100) {
+    methodType = "Unsupervised Learning";
+    if (dataset.numFeatures < 100) {
       console.log("Self-Organising Map");
-      res2 = "Self-Organising Map";
+      bestMethod = "Self-Organising Map";
     } else {
       if (dataset.structure < 0) {
         console.log("Principle Component Analysis");
-        res2 = "Principle Component Analysis";
+        bestMethod = "Principle Component Analysis";
       } else {
         console.log("K-means Clustering");
-        res2 = "K-means Clustering";
+        bestMethod = "K-means Clustering";
       }
     }
   }
@@ -83,42 +51,41 @@ function makeDecision(response) {
   //  Supervised Learning
   else if (dataset.labelsRatio == 1) {
     console.log("Supervised Learning");
-    res2 = "Supervised Learning";
-    if (dataset.isCategorical == 1) {
+    methodType = "Supervised Learning";
+    if (dataset.isCategorical) {
       // Classification
       if (dataset.categories.length >= 3) {
         if (dataset.complexity < 1 && dataset.size >= 1000) {
           console.log("Multiclass Neural Network");
-          res2 = "Multiclass Neural Network";
+          bestMethod = "Multiclass Neural Network";
           //When reached any Neural Network, ask the user "Are there any images involved in the dataset?"
           //if(1) suggest Deep Learning: console.log("Deep Learning"); otherwise unchanged: console.log("Multiclass Neural Network");
           //In the same time, ask the user whether the dataset is a time series,
           //if(1) suggest Recurrent Neural Network and Time Delay Neural Network: console.log("Recurrent Neural Network "), console.log("Time Delay Neural Network");
           //otherwise remain constant: console.log("Multiclass Neural Network");
         } else {
-          console.log("Multiclass Neural Network");
-          res2 = "Multiclass Neural Network";
+          console.log("either Multiclass Neural Network OR Random Forest");
+          bestMethod = "either Multiclass Neural Network OR Random Forest";
           //When reached any Neural Network, ask the user "Are there any images involved in the dataset?"
           //if(1) suggest Deep Learning: console.log("Deep Learning"); otherwise unchanged: console.log("Multiclass Neural Network");
           //In the same time, ask the user whether the dataset is a time series,
           //if(1) suggest Recurrent Neural Network and Time Delay Neural Network: console.log("Recurrent Neural Network "), console.log("Time Delay Neural Network");
           //otherwise remain constant: console.log("Multiclass Neural Network");
-          console.log("Random Forest");
-          res2 = "Random Forest";
         }
       } else {
         if (dataset.relations != null) {
           console.log("Logistic Regression");
-          res2 = "Logistic Regression";
+          bestMethod = "Logistic Regression";
         } else {
           if (dataset.numFeatures < 100) {
             console.log("Multi-Layer Perceptron");
-            res2 = "Multi-Layer Perceptron";
+            bestMethod = "Multi-Layer Perceptron";
           } else {
-            console.log("Naive Bayes");
-            res2 = "Naive Bayes";
-            console.log("Support Vector Machine");
-            res2 = "Support Vector Machine";
+            console.log(
+              "either Naive Bayesian Network OR Support Vector Machine"
+            );
+            bestMethod =
+              "either Naive Bayesian Network OR Support Vector Machine";
             //Is the result of analyse better than guessing(>50%)?
             //If yes use Anti-learning
             //But Chris doesn' t need us to analyse. How could we know the result after analysing?
@@ -133,12 +100,10 @@ function makeDecision(response) {
     else {
       if (dataset.complexity < 1 && dataset.size >= 1000) {
         console.log("Linear Regression");
-        res2 = "Linear Regression";
+        bestMethod = "Linear Regression";
       } else {
-        console.log("Sum Regression");
-        res2 = "Sum Regression";
-        console.log("Random Forest Regression");
-        res2 = "Random Forest Regression";
+        console.log("either Sum Regression OR Random Forest Regression");
+        bestMethod = "either Sum Regression OR Random Forest Regression";
       }
     }
   }
@@ -146,17 +111,22 @@ function makeDecision(response) {
   // Semi-supervised Learning
   else {
     console.log("Semi-supervised Learning");
-    res2 = "Semi-supervised Learning";
+    methodType = "Semi-supervised Learning";
     if (dataset.labelsRatio < 0.33) {
       console.log("Forced Clustering");
-      res2 = "Forced Clustering";
+      bestMethod = "Forced Clustering";
     } else {
       console.log("Self Training");
-      res2 = "Self Training";
+      bestMethod = "Self Training";
     }
   }
-  res += res2;
+  const res = `After analysis, it would appear the dataset would best be modelled using ${methodType}. The algorithm suggests that the best Machine Learning algorithm to use on ${getDatasetName()} is </br> ${bestMethod}`;
   result.innerHTML = res;
-  document.querySelector("#features").appendChild(dataset);
+  // TODO: Make a list of features
+  document.querySelector("#features").innerHTML = JSON.stringify(
+    dataset,
+    " ",
+    2
+  );
   document.querySelector("#result").appendChild(resultValue);
 }
