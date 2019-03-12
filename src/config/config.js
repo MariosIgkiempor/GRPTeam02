169bb68ab0e02733d7d22e98c85633207af7aaec
Marios Igkiempor <marioigkiempor@gmail.com>
2019-03-12 19:47:30 +0800

    changed mlab db to US

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,8 @@
 // and other config variables needed throughout the app
 
 module.exports = {
-  mongoURI: 'mongodb://admin:password1@ds219832.mlab.com:19832/grpteam02-db',
+  // mongoURI: 'mongodb://admin:password1@ds219832.mlab.com:19832/grpteam02-db',
+  mongoURI: 'mongodb://admin:password1@ds237669.mlab.com:37669/grpteam02-db2',
   // mongoURI:
   //   'mongodb+srv://admin:p37LevejJ0grLyZB@grpteam02cluster-16frt.azure.mongodb.net/test?retryWrites=true',
   port: process.env.PORT ? process.env.PORT : 8000,
