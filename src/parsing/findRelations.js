3949318c71251503469bc01bbc1ae066f3202b7f
Marios Igkiempor <marioigkiempor@gmail.com>
2019-03-17 15:03:50 +0800

    added covariances of each piar of columns as a measure of relations

---
+++ src/parsing/findRelations.js
@@ -0,0 +1,31 @@
+const R = require('ramda')
+
+const findRelations = function (data, threshold) {
+  const columns = R.transpose(data)
+
+  const pairs = columns.reduce(
+    (acc, val, i1) => [
+      ...acc,
+      ...new Array(columns.length - 1 - i1)
+        .fill(0)
+        .map((v, i2) => [columns[i1], columns[i1 + 1 + i2]])
+    ],
+    []
+  )
+
+  // Covariance between each pair of columns as a measure of relation
+  const covariances = pairs.map(pair => {
+    const meanX = R.mean(pair[0]) // Mx
+    const meanY = R.mean(pair[1]) // My
+    const xs = R.map(x => x - meanX)(pair[0]) // X - Mx
+    const ys = R.map(y => y - meanY)(pair[1]) // Y - My
+    const xsys = xs.map((x, i) => x * ys[i]) // (X - Mx)(Y - My)
+    const sumXsys = R.sum(xsys)
+    const covariance = sumXsys / (pairs[0].length - 1)
+    return covariance
+  })
+
+  return covariances
+}
+
+console.log(findRelations([[1, 2], [1, 2], [1, 2], [1, 2]], 0.5))
