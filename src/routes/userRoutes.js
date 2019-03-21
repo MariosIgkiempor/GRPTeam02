2781c06fd4c6e3dc0a98a80859d41df22cf7ca70
Marios Igkiempor <marioigkiempor@gmail.com>
2019-03-21 17:18:12 +0800

    added error checking to register route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const emailValidator = require('email-validator')
 const User = require('../models/User') // bring in the User model
 
 router.post('/register/', (req, res) => {
-  console.log(req.body)
+  const { username, email, password, password2 } = req.body
+
+  let errors
+
+  if (!(username && email && password && password2)) {
+    errors.push('Please fill in all fields')
+  }
+  if (password.length < 6) {
+    errors.push('Password must be at least 6 characters long')
+  }
+  if (password !== password2) {
+    errors.push('Passwords do not match')
+  }
+  if (!emailValidator.validate(email)) {
+    errors.push('Please enter a valid email')
+  }
+
+  if (errors.length === 0) {
+    res.send('success')
+  } else {
+    res.send(errors)
+  }
 })
 
 module.exports = router
